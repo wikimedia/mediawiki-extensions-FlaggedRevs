83807c49601657fb2f563f67623571ce38f2c3fe
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-10-09 22:51:36 +0000

    Localisation updates from Betawiki. * Fixes and additions to 17 extensions for bcl, fo, hsb, is, it

--- FlaggedRevsPage.i18n.php
+++ FlaggedRevsPage.i18n.php
@@ -770,6 +770,21 @@ $RevisionreviewMessages['hsb'] = array(
 	'stabilization-def-short-1' => 'Stabilny',
 );
 
+$RevisionreviewMessages['it'] = array(
+	'reviewer'              => 'Revisore',
+	'group-reviewer'        => 'Revisori',
+	'group-reviewer-member' => 'Revisore',
+	'grouppage-reviewer'    => '{{ns:project}}:Revisore',
+	'revreview-current'     => 'Bozza',
+	'tooltip-ca-current'    => 'Vedi la bozza attuale di questa pagina',
+	'revreview-edit'        => 'Modifica la bozza',
+	'revreview-source'      => 'Codice sorgente della bozza',
+	'revreview-stable'      => 'Stabile',
+	'tooltip-ca-stable'     => 'Vedi la versione stabile di questa voce',
+	'revreview-oldrating'   => 'È stata giudicata:',
+	'revreview-noflagged'   => 'Non ci sono revisioni convalidate di questa voce, perciò potrebbe \'\'\'non\'\'\' essere stata [[Aiuto:Convalida delle voci|controllata]] la sua qualità.',
+);
+
 $RevisionreviewMessages['la'] = array(
 	'editor'                => 'Recensor',
 	'group-editor'          => 'Recensores',
