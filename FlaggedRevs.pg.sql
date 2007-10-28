41d4a9bd88c6e1f4dadac5b6272ed9cef72106ad
Aaron Schulz <aaron@users.mediawiki.org>
2007-10-28 19:39:52 +0000

    *Update pg schema

--- FlaggedRevs.pg.sql
+++ FlaggedRevs.pg.sql
@@ -6,44 +6,46 @@ BEGIN;
 CREATE TABLE flaggedrevs (
   fr_namespace    SMALLINT NOT NULL DEFAULT 0,
   fr_title        TEXT     NOT NULL DEFAULT '',
-  fr_rev_id       INTEGER  NOT NULL PRIMARY KEY DEFAULT 0 ,
+  fr_rev_id       INTEGER  NOT NULL DEFAULT 0 ,
   fr_user int(5)  INTEGER      NULL REFERENCES mwuser(user_id) ON DELETE SET NULL,
   fr_timestamp    TIMESTAMPTZ,
   fr_comment      TEXT     NOT NULL DEFAULT '',
   fr_quality      SMALLINT NOT NULL DEFAULT 0,
   fr_text         TEXT     NOT NULL DEFAULT '',
-  fr_flags        TEXT     NOT NULL
+  fr_flags        TEXT     NOT NULL,
+  PRIMARY KEY (fr_namespace,fr_title,fr_rev_id)
 );
+CREATE UNIQUE INDEX fr_rev_id ON flaggedrevs (fr_rev_id);
 CREATE INDEX fr_namespace_title ON flaggedrevs (fr_namespace,fr_title,fr_quality,fr_rev_id);
 
 CREATE TABLE flaggedpages (
-  fp_page_id INTEGER NOT NULL PRIMARY KEY DEFAULT 0,
-  fp_select INTEGER NOT NULL DEFAULT 0,
-  fp_override bool NOT NULL
+  fp_page_id   INTEGER NOT NULL PRIMARY KEY DEFAULT 0,
+  fp_select    INTEGER NOT NULL DEFAULT 0,
+  fp_override  bool NOT NULL
 )
 
 CREATE TABLE flaggedrevtags (
   frt_rev_id     INTEGER  NOT NULL DEFAULT 0 ,
   frt_dimension  TEXT     NOT NULL DEFAULT '',
-  frt_value      SMALLINT NOT NULL DEFAULT 0
+  frt_value      SMALLINT NOT NULL DEFAULT 0,
+  PRIMARY KEY (frt_rev_id,frt_dimension)
 );
-CREATE UNIQUE INDEX frt_rev_dimension ON flaggedrevtags (frt_rev_id,frt_dimension);
 
 CREATE TABLE flaggedtemplates (
   ft_rev_id      INTEGER  NOT NULL DEFAULT 0 ,
   ft_namespace   SMALLINT NOT NULL DEFAULT 0,
   ft_title       TEXT     NOT NULL DEFAULT '',
-  ft_tmp_rev_id  INTEGER  NOT NULL DEFAULT 0
+  ft_tmp_rev_id  INTEGER  NOT NULL DEFAULT 0,
+  PRIMARY KEY (ft_rev_id,ft_namespace,ft_title)
 );
-CREATE UNIQUE INDEX ft_rev_namespace_title ON flaggedtemplates (ft_rev_id,ft_namespace,ft_title);
 
 CREATE TABLE flaggedimages (
   fi_rev_id         INTEGER   NOT NULL DEFAULT 0 ,
   fi_name           TEXT      NOT NULL PRIMARY KEY,
   fi_img_timestamp  TIMESTAMPTZ,
-  fi_img_sha1       CHAR(64)
+  fi_img_sha1       CHAR(64),
+  PRIMARY KEY (fi_rev_id,fi_name)
 );
-CREATE UNIQUE INDEX fi_rev_name        ON flaggedimages (fi_rev_id,fi_name);
 
 ALTER TABLE page 
 	ADD page_ext_reviewed bool NULL,
