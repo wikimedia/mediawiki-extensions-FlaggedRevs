1f952e972a26b53d3ab9850a3b97447ec58e88b6
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-28 18:02:28 +0000

    Localisation updates for extension messages from Betawiki (2007-12-28 18.55 CET)

---
+++ Language/MakeReviewer.i18n.stq.php
@@ -0,0 +1,9 @@
+<?php
+/** Seeltersk (Seeltersk)
+ * @author Pyt
+ */
+$messages = array(
+	'makereviewer-autosum' => 'automatiske Gjuchte-uutgoawe',
+	'rights-editor-grant'  => 'roate dät Sieuwer-Gjucht an [[$1]]',
+	'rights-editor-revoke' => 'äntlook dät Sieuwer-Gjucht fon [[$1]]',
+);
