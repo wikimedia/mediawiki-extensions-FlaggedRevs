5ec422d0013afaaec0939b381ceaf629fad47a13
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-29 15:22:11 +0000

    Localisation updates for extension messages from Betawiki (2007-12-29 15:25 CET)

--- Language/FlaggedRevsPage.i18n.bg.php
+++ Language/FlaggedRevsPage.i18n.bg.php
@@ -3,6 +3,9 @@
  * @author DCLXVI
  */
 $messages = array(
+	'editor'                => 'Редактор',
+	'group-editor'          => 'Редактори',
+	'group-editor-member'   => 'Редактор',
 	'stableversions-page'   => 'Име на страницата:',
 	'stabilization-page'    => 'Име на страницата:',
 	'stabilization-comment' => 'Коментар:',
--- Language/MakeReviewer.i18n.stq.php
+++ Language/MakeReviewer.i18n.stq.php
@@ -3,7 +3,34 @@
  * @author Pyt
  */
 $messages = array(
-	'makereviewer-autosum' => 'automatiske Gjuchte-uutgoawe',
-	'rights-editor-grant'  => 'roate dät Sieuwer-Gjucht an [[$1]]',
-	'rights-editor-revoke' => 'äntlook dät Sieuwer-Gjucht fon [[$1]]',
+	'makereviewer'                  => 'Sieuwer-/Wröig-Gjucht reeke/äntluuke',
+	'makereviewer-header'           => '<strong>Mäd dit Formular konnen Administratore un Bürokrate Benutsere dät Gjucht tou Artikkelpröiwenge reeke.</strong><br /> 
+	Reek dän Benutsernoome in dät Fäild ien un klik ap ju Schaltfläche, uum dät Gjucht tou sätten.
+	Truch Reeken fon dät Wröiggjucht wäd automatisk uk dät Sieuwer-Gjucht roat. Dät Äntluuken fon dät Sieuwer-Gjucht häd automatisk dät Äntluuken fon dät Wröiggjucht as Foulge.',
+	'makereviewer-username'         => 'Benutsernoome:',
+	'makereviewer-search'           => 'Stoatus oufräigje',
+	'makereviewer-iseditor'         => '[[User:$1|$1]] häd dät Sieuwer-Gjucht.',
+	'makereviewer-noteditor'        => '[[User:$1|$1]] häd neen Sieuwer-Gjucht.',
+	'makereviewer-isvalidator'      => '[[User:$1|$1]] häd dät Wröiggjucht.',
+	'makereviewer-notvalidator'     => '[[User:$1|$1]] häd neen Wröiggjucht.',
+	'makereviewer-legend'           => 'Benutsergjuchte annerje',
+	'makereviewer-change-e'         => 'Sieuwer-Gjucht:',
+	'makereviewer-change-r'         => 'Wröiggjucht:',
+	'makereviewer-grant1'           => 'Reeke',
+	'makereviewer-revoke1'          => 'Äntluuke',
+	'makereviewer-grant2'           => 'Reeke',
+	'makereviewer-revoke2'          => 'Äntluuke',
+	'makereviewer-comment'          => 'Kommentoar:',
+	'makereviewer-granted-e'        => '[[User:$1|$1]] häd nu dät Sieuwer-Gjucht.',
+	'makereviewer-revoked-e'        => '[[User:$1|$1]] wuude dät Sieuwer-Gjucht äntleeken.',
+	'makereviewer-granted-r'        => '[[User:$1|$1]] häd nu dät Wröiggjucht.',
+	'makereviewer-revoked-r'        => '[[User:$1|$1]] wuude dät Wröiggjucht äntleeken.',
+	'makereviewer-logpage'          => 'Sieuwer-/Wröig-Gjuchte-Logbouk',
+	'makereviewer-logentrygrant-e'  => 'roate dät Sieuwer-Gjucht an [[$1]]',
+	'makereviewer-logentryrevoke-e' => 'äntlook dät Sieuwer-Gjucht fon [[$1]]',
+	'makereviewer-logentrygrant-r'  => 'roate dät Wröiggjucht an [[$1]]',
+	'makereviewer-logentryrevoke-r' => 'äntlook dät Wröiggjucht fon [[$1]]',
+	'makereviewer-autosum'          => 'automatiske Gjuchte-uutgoawe',
+	'rights-editor-grant'           => 'roate dät Sieuwer-Gjucht an [[$1]]',
+	'rights-editor-revoke'          => 'äntlook dät Sieuwer-Gjucht fon [[$1]]',
 );
