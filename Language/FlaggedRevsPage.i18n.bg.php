054066f7da086e946871a95b856873a415bef14b
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-31 09:19:53 +0000

    Localisation updates for extension messages from Betawiki (2007-12-31 9:36 CET)

--- Language/FlaggedRevsPage.i18n.bg.php
+++ Language/FlaggedRevsPage.i18n.bg.php
@@ -7,6 +7,8 @@ $messages = array(
 	'group-editor'          => 'Редактори',
 	'group-editor-member'   => 'Редактор',
 	'stableversions-page'   => 'Име на страницата:',
+	'unreviewed-category'   => 'Категория:',
+	'unreviewed-diff'       => 'Промени',
 	'stabilization-page'    => 'Име на страницата:',
 	'stabilization-comment' => 'Коментар:',
 );
---
+++ Language/MakeReviewer.i18n.bg.php
@@ -0,0 +1,12 @@
+<?php
+/** Bulgarian (Български)
+ * @author DCLXVI
+ */
+$messages = array(
+	'makereviewer-username' => 'Име на потребителя:',
+	'makereviewer-grant1'   => 'Предоставяне',
+	'makereviewer-revoke1'  => 'Отнемане',
+	'makereviewer-grant2'   => 'Предоставяне',
+	'makereviewer-revoke2'  => 'Отнемане',
+	'makereviewer-comment'  => 'Коментар:',
+);
