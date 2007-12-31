20c1ee325f729d7aa9eef8ebab7b56c10a4e7cb4
Bertrand Grondin <grondin@users.mediawiki.org>
2007-12-31 23:34:06 +0000

    Localisation updates for extensions from Betawiki (2007-12-31 19:00 UTC)

---
+++ Language/FlaggedRevsPage.i18n.lb.php
@@ -0,0 +1,15 @@
+<?php
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages = array(
+	'stableversions'            => 'Stabil Versioun',
+	'stableversions-page'       => 'Säitenumm:',
+	'unreviewed-category'       => 'Kategorie:',
+	'unreviewed-diff'           => 'Ännerungen',
+	'stabilization-page'        => 'Säitennumm:',
+	'stabilization-comment'     => 'Bemierkung:',
+	'stabilization-sel-short-0' => 'Qualitéit',
+	'stabilization-sel-short-1' => 'Keng',
+);
---
+++ Language/MakeReviewer.i18n.lb.php
@@ -0,0 +1,8 @@
+<?php
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages = array(
+	'makereviewer-username' => 'Benotzernumm:',
+	'makereviewer-search'   => 'Status offroen',
+);
