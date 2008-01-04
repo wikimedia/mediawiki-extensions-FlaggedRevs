36dbb3b3632b86cf21f35412e15049f028c72b6a
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-01-04 18:35:36 +0000

    Localisation updates for extension messages from Betawiki (2008-01-04 19:09 CET)

---
+++ Language/FlaggedRevsPage.i18n.vo.php
@@ -0,0 +1,16 @@
+<?php
+/** Volapük (Volapük)
+ * @author Malafaya
+ */
+$messages = array(
+	'revreview-toggle'          => '(+/-)',
+	'revreview-auto'            => '(itjäfidik)',
+	'revreview-style-2'         => 'Gudik',
+	'revreview-style-3'         => 'Naböfik',
+	'revreview-log'             => 'Küpet:',
+	'stableversions-page'       => 'Nem pada:',
+	'unreviewed-category'       => 'Klad:',
+	'unreviewed-diff'           => 'Votükams',
+	'stabilization-page'        => 'Nem pada:',
+	'stabilization-sel-short-1' => 'Nonik',
+);
