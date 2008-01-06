f9a69b84670579b000c4627877722a3f57d8f523
Bertrand Grondin <grondin@users.mediawiki.org>
2008-01-06 19:48:29 +0000

    Localisation updates for extensions from Betawiki (2008-01-06 19:23 UTC)

--- Language/MakeReviewer.i18n.hu.php
+++ Language/MakeReviewer.i18n.hu.php
@@ -4,10 +4,24 @@
  * @author KossuthRad
  */
 $messages = array(
-	'makereviewer-username' => 'A User neve:',
-	'makereviewer-search'   => 'Menj',
-	'makereviewer-legend'   => 'Megváltoztatni a User jogait',
-	'makereviewer-change-e' => 'Szerkesztői státusz:',
-	'makereviewer-comment'  => 'Komment:',
+	'makereviewer'              => 'Kinevezett/lefokozott szerkesztők',
+	'makereviewer-username'     => 'A User neve:',
+	'makereviewer-search'       => 'Menj',
+	'makereviewer-iseditor'     => '[[User:$1|$1]] -nek szerkesztői státusza van.',
+	'makereviewer-noteditor'    => '[[User:$1|$1]] -nek nincs szerkesztői státusza.',
+	'makereviewer-isvalidator'  => '[[User:$1|$1]] -nek van vizsgáló státusza.',
+	'makereviewer-notvalidator' => '[[User:$1|$1]] -nek nincs továbbiakban vizsgáló státusza.',
+	'makereviewer-legend'       => 'Megváltoztatni a User jogait',
+	'makereviewer-change-e'     => 'Szerkesztői státusz:',
+	'makereviewer-change-r'     => 'Vizsgáló státusza:',
+	'makereviewer-grant1'       => 'hozzájárul',
+	'makereviewer-revoke1'      => 'Érvénytelenít',
+	'makereviewer-grant2'       => 'hozzájárul',
+	'makereviewer-revoke2'      => 'Érvénytelenít',
+	'makereviewer-comment'      => 'Komment:',
+	'makereviewer-granted-e'    => '[[User:$1|$1]] -nek most van szerkesztői státusza.',
+	'makereviewer-revoked-e'    => '[[User:$1|$1]] -nek nincs továbbiakban szerkesztői státusza.',
+	'makereviewer-granted-r'    => '[[User:$1|$1]] -nek most van vizsgáló státusza.',
+	'makereviewer-revoked-r'    => '[[User:$1|$1]] -nek nincs továbbiakban vizsgáló státusza.',
+	'rights-editor-revoke'      => 'A [[$1]]-ből elmozgatott szerkesztő státusz',
 );
-
