bacf74049fc89ff33bab9ea26a4718589a77806a
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-01-16 00:06:16 +0000

    Localisation updates for extension messages from Betawiki (2008-01-16 0:44 CET)

---
+++ Language/FlaggedRevsPage.i18n.myv.php
@@ -0,0 +1,9 @@
+<?php
+/** Erzya (эрзянь кель)
+ * @author Amdf
+ */
+$messages = array(
+	'stableversions-page' => 'Лопань лем:',
+	'unreviewed-category' => 'Категория:',
+	'stabilization-page'  => 'Лопань лем:',
+);
