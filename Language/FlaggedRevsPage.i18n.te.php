6885de08ae65a3853069f143f365070792fd35c0
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-01-21 16:57:03 +0000

    Localisation updates for extension messages from Betawiki (2008-01-21 17:19 CET)

---
+++ Language/FlaggedRevsPage.i18n.te.php
@@ -0,0 +1,18 @@
+<?php
+/** Telugu (తెలుగు)
+ * @author Veeven
+ */
+$messages = array(
+	'hist-quality'          => '[నాణ్యత]',
+	'revreview-accuracy'    => 'ఖచ్చితత్వం',
+	'revreview-accuracy-2'  => 'ఖచ్చితం',
+	'revreview-depth-1'     => 'ప్రాథమిక',
+	'revreview-log'         => 'వ్యాఖ్య:',
+	'stableversions-page'   => 'పేజీ పేరు:',
+	'unreviewed-category'   => 'వర్గం:',
+	'unreviewed-diff'       => 'మార్పులు',
+	'stabilization-page'    => 'పేజీ పేరు:',
+	'stabilization-comment' => 'వ్యాఖ్య:',
+	'reviewedpages-none'    => 'ఈ జాబితాలో పేజీలు లేవు.',
+);
+
---
+++ Language/MakeReviewer.i18n.te.php
@@ -0,0 +1,8 @@
+<?php
+/** Telugu (తెలుగు)
+ * @author Veeven
+ */
+$messages = array(
+	'makereviewer-comment' => 'వ్యాఖ్య:',
+);
+
