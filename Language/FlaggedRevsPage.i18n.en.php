af524507de5bd92107cc06a04cfb37b5ecc6c334
Aaron Schulz <aaron@users.mediawiki.org>
2008-02-03 05:15:44 +0000

    Remove ID

--- Language/FlaggedRevsPage.i18n.en.php
+++ Language/FlaggedRevsPage.i18n.en.php
@@ -87,7 +87,7 @@ $messages = array(
 	'revreview-text'       => "Stable versions are set as the default content on page view rather than the newest revision.",
 	'revreview-toolow'     => 'You must at least rate each of the below attributes higher than "unapproved" in order 
 	for a revision to be considered reviewed. To depreciate a revision, set all fields to "unapproved".',
-	'revreview-flag'       => 'Review this revision (#$1)',
+	'revreview-flag'       => 'Review this revision',
 	'revreview-legend'     => 'Rate revision content',
 	'revreview-notes'      => 'Observations or notes to display:',
 	'revreview-accuracy'   => 'Accuracy',
