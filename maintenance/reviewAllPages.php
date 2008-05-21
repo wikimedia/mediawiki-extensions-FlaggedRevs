df3e40d76a8f30e444fbc417b0eb729822afb417
Aaron Schulz <aaron@users.mediawiki.org>
2008-05-21 00:14:22 +0000

    Pass in <userid>

--- maintenance/reviewAllPages.php
+++ maintenance/reviewAllPages.php
@@ -4,8 +4,22 @@
 require dirname(__FILE__) . '/../../../maintenance/commandLine.inc';
 require dirname(__FILE__) . '/reviewAllPages.inc';
 
+if( isset($options['help']) || !isset($args[0]) ) {
+	echo <<<TEXT
+Usage:
+    php refreshLinks.php --help
+    php refreshLinks.php <username>
+
+    --help               : This help message
+    --<userid>           : The ID of the existing user to use as the "reviewer" (you can find your ID at Special:Preferences)
+
+TEXT;
+	exit(0);
+}
+
 error_reporting( E_ALL & (~E_NOTICE) );
 
 $db = wfGetDB( DB_MASTER );
+$user = User::newFromId( intval($args[0]) );
 
-autoreview_current( $db );
+autoreview_current( $user, $db );
