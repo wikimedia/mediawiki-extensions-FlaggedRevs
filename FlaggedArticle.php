a7b285ea3ff57158b26bfd610e9be834d83125fc
Aaron Schulz <aaron@users.mediawiki.org>
2008-07-23 19:42:02 +0000

    lowProfileUI() tweak

--- FlaggedArticle.php
+++ FlaggedArticle.php
@@ -347,7 +347,7 @@ class FlaggedArticle extends Article {
 	   			}
 				$synced = FlaggedRevs::stableVersionIsSynced( $frev, $this->parent, $parserOut, null );
 				# Construct some tagging
-				if( !$wgOut->isPrintable() && !FlaggedRevs::lowProfileUI() ) {
+				if( !$wgOut->isPrintable() && !(FlaggedRevs::lowProfileUI() && $synced) ) {
 					$class = $quality ? 'fr-icon-quality' : 'fr-icon-stable';
 					$tooltip = $quality ? 'revreview-quality-title' : 'revreview-stable-title';
 					$tooltip = wfMsgHtml($tooltip);
