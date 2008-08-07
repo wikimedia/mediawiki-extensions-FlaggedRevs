732ae40b8439b940673963f7d1ced051bd3a3137
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-08-07 23:42:33 +0000

    * add ValidationStatistics to Translate * cleanup ValidationStatistics.i18n.php

--- language/ValidationStatistics.i18n.php
+++ language/ValidationStatistics.i18n.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Internationalisation file for FlaggedRevs extension, section RatingHistory
+ * Internationalisation file for FlaggedRevs extension, section ValidationStatistics
  *
  * @addtogroup Extensions
  */
@@ -9,10 +9,10 @@ $messages = array();
 
 $messages['en'] = array(
 	'validationstatistics'        => 'Validation statistics',
-	'validationstatistics-users'  => '\'\'\'{{SITENAME}}\'\'\' currently has \'\'\'$1\'\'\' {{PLURAL:$1|user|users}} with [[{{MediaWiki:Validationpage}}|Editor]] rights 
-	and \'\'\'$2\'\'\' {{PLURAL:$2|user|users}} with [[{{MediaWiki:Validationpage}}|Reviewer]] rights.',
+	'validationstatistics-users'  => '\'\'\'{{SITENAME}}\'\'\' currently has \'\'\'$1\'\'\' {{PLURAL:$1|user|users}} with [[{{MediaWiki:Validationpage}}|Editor]] rights
+and \'\'\'$2\'\'\' {{PLURAL:$2|user|users}} with [[{{MediaWiki:Validationpage}}|Reviewer]] rights.',
 	'validationstatistics-table'  => "Statistics for each namespace are shown below, excluding redirects pages.
-	
+
 '''Note:''' the following data is cached for several hours and may not be up to date.",
 	'validationstatistics-ns'     => 'Namespace',
 	'validationstatistics-total'  => 'Pages',
