ccb45b6de8147cca6563c914bc5008636e1299cd
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-10-01 23:07:07 +0000

    established -> authorised

--- language/FlaggedRevs.i18n.php
+++ language/FlaggedRevs.i18n.php
@@ -74,7 +74,7 @@ Refreshing the page and rereviewing can solve this problem.',
 	'revreview-edit'               => 'Edit draft',
 	'revreview-editnotice'         => '\'\'\'Note: Edits to this page will be incorporated into the [[{{MediaWiki:Validationpage}}|stable version]] once an authorised user reviews them.\'\'\'',
 	'revreview-flag'               => 'Review this revision',
-	'revreview-edited'             => '\'\'\'Edits will be incorporated into the [[{{MediaWiki:Validationpage}}|stable version]] once an established user reviews them.\'\'\'
+	'revreview-edited'             => '\'\'\'Edits will be incorporated into the [[{{MediaWiki:Validationpage}}|stable version]] once an authorised user reviews them.\'\'\'
 \'\'\'The \'\'draft\'\' is shown below.\'\'\' [{{fullurl:{{FULLPAGENAMEE}}|oldid=$1&diff=cur&diffonly=0}} $2 {{PLURAL:$2|change awaits|changes await}}] review.',
 	'revreview-flag'               => 'Review this revision',
 	'revreview-invalid'            => '\'\'\'Invalid target:\'\'\' no [[{{MediaWiki:Validationpage}}|reviewed]] revision corresponds to the given ID.',
