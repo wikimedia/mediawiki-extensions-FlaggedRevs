b55b9629c4349bd2e24a200a3be062d1a6b1a706
Tim Starling <tstarling@users.mediawiki.org>
2008-12-30 08:31:28 +0000

    Added theoretical reversibility

--- maintenance/clearCachedText.php
+++ maintenance/clearCachedText.php
@@ -9,6 +9,7 @@ $IP = getenv( 'MW_INSTALL_PATH' );
 if ( strval( $IP ) == '' ) {
 	$IP = dirname( __FILE__ ) . '/../../..';
 }
+$optionsWithArgs = array( 'backup' );
 require( "$IP/maintenance/commandLine.inc" );
 
 $pageId = 0;
@@ -18,6 +19,17 @@ $dbr = wfGetDB( DB_SLAVE );
 $dbw = wfGetDB( DB_MASTER );
 $batchSize = 1000;
 $maxPage = $dbr->selectField( 'flaggedrevs', 'MAX(fr_page_id)', '', __METHOD__ );
+
+if ( !isset( $options['backup'] ) ) {
+	echo "Usage: clearCachedText.php --backup=<file>\n";
+	exit( 1 );
+}
+
+$backupFile = fopen( $options['backup'], 'w' );
+if ( !$backupFile ) {
+	echo "Unable to open backup file\n";
+	exit( 1 );
+}
 
 while ( true ) {
 	$res = $dbr->select( 'flaggedrevs', '*', 
@@ -31,6 +43,9 @@ while ( true ) {
 	}
 	foreach ( $res as $row ) {
 		$flags = explode( ',', $row->fr_flags );
+		$backupRecord = array( $row->fr_page_id, $row->fr_rev_id, $row->fr_flags, $row->fr_text );
+		fwrite( $backupFile, implode( "\t", array_map( 'rawurlencode', $backupRecord ) ) . "\n" );
+
 		$dbw->update( 'flaggedrevs', 
 			array( /* SET */
 				'fr_text' => '',
