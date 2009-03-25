44e8a58b1b7ca7bdca0b563c16daaa31b2c4f4e4
Aaron Schulz <aaron@users.mediawiki.org>
2009-03-25 14:20:52 +0000

    Don't show filter if there is only one level

--- specialpages/QualityOversight_body.php
+++ specialpages/QualityOversight_body.php
@@ -70,7 +70,10 @@ class QualityOversight extends SpecialPage
 			'<fieldset><legend>' . wfMsgHtml('qualityoversight-legend') . '</legend><p>' .
 			Xml::hidden( 'title', $wgTitle->getPrefixedDBKey() ) .
 			FlaggedRevsXML::getNamespaceMenu( $this->namespace ) . '&nbsp;' .
-			FlaggedRevsXML::getLevelMenu( $this->level, 'revreview-filter-all', 1 ) . '&nbsp;' .
+			( FlaggedRevs::qualityVersions() ?
+				FlaggedRevsXML::getLevelMenu( $this->level, 'revreview-filter-all', 1 ) . '&nbsp;'
+				: ""
+			) .
 			Xml::inputLabel( wfMsg( 'specialloguserlabel' ), 'user', 'user', 20, $this->user ) . '<br/>' .
 			FlaggedRevsXML::getStatusFilterMenu( $this->status ) . '&nbsp;' .
 			FlaggedRevsXML::getAutoFilterMenu( $this->automatic ) . '&nbsp;' .
