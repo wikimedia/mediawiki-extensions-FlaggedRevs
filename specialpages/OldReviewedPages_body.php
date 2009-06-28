1360b38769fbe22a75a8a820ad8847f25f5ff9d1
Aaron Schulz <aaron@users.mediawiki.org>
2009-06-28 15:52:09 +0000

    Reverted r51828 since some sites customized the msg with some nifty links (bug 19425)

--- specialpages/OldReviewedPages_body.php
+++ specialpages/OldReviewedPages_body.php
@@ -92,6 +92,7 @@ class OldReviewedPages extends SpecialPage
 			$wgOut->addHTML( $form );
 			# Add list output
 			$num = $pager->getNumRows();
+			$wgOut->addHTML( wfMsgExt('oldreviewedpages-list', array('parse'), $num ) );
 			if( $num ) {
 				$wgOut->addHTML( $pager->getNavigationBar() );
 				$wgOut->addHTML( $pager->getBody() );
