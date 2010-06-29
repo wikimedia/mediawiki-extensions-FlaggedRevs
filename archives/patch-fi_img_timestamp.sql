0b7a04c06f9f7a240730516b9e5db4d67ac7bfb7
Aaron Schulz <aaron@users.mediawiki.org>
2010-06-29 20:02:49 +0000

    Catch '' as well as '0'

--- archives/patch-fi_img_timestamp.sql
+++ archives/patch-fi_img_timestamp.sql
@@ -3,4 +3,4 @@ ALTER TABLE /*$wgDBprefix*/flaggedimages
     CHANGE fi_img_timestamp fi_img_timestamp char(14) NULL;
 -- Move bad values over to NULL
 UPDATE /*$wgDBprefix*/flaggedimages
-    SET fi_img_timestamp = NULL WHERE fi_img_timestamp = '0\0\0\0\0\0\0\0\0\0\0\0\0\0';
+    SET fi_img_timestamp = NULL WHERE LOCATE( '\0', fi_img_timestamp );
