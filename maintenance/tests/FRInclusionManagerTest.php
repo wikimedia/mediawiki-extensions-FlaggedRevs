989025045511d9a761f2ce529828a11bac8036a8
Aaron Schulz <aaron@users.mediawiki.org>
2010-07-24 21:24:20 +0000

    * Renamed vars * Added some tests

--- maintenance/tests/FRInclusionManagerTest.php
+++ maintenance/tests/FRInclusionManagerTest.php
@@ -38,42 +38,46 @@ class MWException extends Exception { } // just in case
 
 class FRInclusionManagerTest extends PHPUnit_Framework_TestCase {
 	/* starting input */
-	protected static $inputTmps = array(
+	protected static $inputTemplates = array(
 		10 	=> array('XX' => '1242', 'YY' => '0'),
 		4 	=> array('cite' => '30', 'moo' => 0),
-		0 	=> array('ZZ' => 464)
+		0 	=> array('ZZ' => 464, '0' => 13)
 	);
 	protected static $inputFiles = array(
 		'fXX' => array('ts' => '20100405192110', 'sha1' => 'abc1'),
 		'fYY' => array('ts' => '20000403101300', 'sha1' => 'ffc2'),
 		'fZZ' => array('ts' => '0', 'sha1' => ''),
-		'fle' => array('ts' => 0, 'sha1' => '')
+		'fle' => array('ts' => 0, 'sha1' => ''),
+		'0'   => array('ts' => '20000203101350', 'sha1' => 'ae33'),
 	);
-	/* output to test against (test# => test) */
-	protected static $rOutputTmps = array(
+	/* output to test against (test# => <NS,dbkey,expected rev ID>) */
+	protected static $reviewedOutputTemplates = array(
 		0 => array( 10, 'XX', 1242 ),
 		1 => array( 10, 'YY', 0 ),
 		2 => array( 4, 'cite', 30 ),
 		3 => array( 4, 'moo', 0 ),
 		4 => array( 0, 'ZZ', 464 ),
-		5 => array( 0, 'notexists', null )
+		5 => array( 0, 'notexists', null ),
+		6 => array( 0, '0', 13 ),
 	);
-	protected static $rOutputFiles = array(
+	protected static $stableOutputTemplates = array(
+		0 => array( 10, 'XX', 1242 ),
+		1 => array( 10, 'YY', 0 ),
+		2 => array( 4, 'cite', 30 ),
+		3 => array( 4, 'moo', 0 ),
+		4 => array( 0, 'ZZ', 464 ),
+		5 => array( 0, 'notexists', 0 ),
+		6 => array( 0, '0', 13 ),
+	);
+	/* output to test against (test# => <dbkey,expected TS,expected sha1>) */
+	protected static $reviewedOutputFiles = array(
 		0 => array( 'fXX', '20100405192110', 'abc1'),
 		1 => array( 'fYY', '20000403101300', 'ffc2'),
 		2 => array( 'fZZ', '0', ''),
 		3 => array( 'fle', '0', ''),
 		4 => array( 'notgiven', null, null),
 	);
-	protected static $sOutputTmps = array(
-		0 => array( 10, 'XX', 1242 ),
-		1 => array( 10, 'YY', 0 ),
-		2 => array( 4, 'cite', 30 ),
-		3 => array( 4, 'moo', 0 ),
-		4 => array( 0, 'ZZ', 464 ),
-		5 => array( 0, 'notexists', 0 )
-	);
-	protected static $sOutputFiles = array(
+	protected static $stableOutputFiles = array(
 		0 => array( 'fXX', '20100405192110', 'abc1'),
 		1 => array( 'fYY', '20000403101300', 'ffc2'),
 		2 => array( 'fZZ', '0', ''),
@@ -108,15 +112,15 @@ class FRInclusionManagerTest extends PHPUnit_Framework_TestCase {
 
 	public function testManagerClear() {
 		$im = FRInclusionManager::singleton();
-		$im->setReviewedVersions( self::$inputTmps, self::$inputFiles );
+		$im->setReviewedVersions( self::$inputTemplates, self::$inputFiles );
 		$im->clear();
 		$this->assertEquals( false, $im->parserOutputIsStabilized(), "Empty on clear()" );
 	}
 
 	public function testReviewedTemplateVersions() {
 		$im = FRInclusionManager::singleton();
-		$im->setReviewedVersions( self::$inputTmps, self::$inputFiles );
-		foreach ( self::$rOutputTmps as $x => $triple ) {
+		$im->setReviewedVersions( self::$inputTemplates, self::$inputFiles );
+		foreach ( self::$reviewedOutputTemplates as $x => $triple ) {
 			list($ns,$dbKey,$expId) = $triple;
 			$title = Title::makeTitleSafe( $ns, $dbKey );
 			$actual = $im->getReviewedTemplateVersion( $title );
@@ -126,8 +130,8 @@ class FRInclusionManagerTest extends PHPUnit_Framework_TestCase {
 
 	public function testReviewedFileVersions() {
 		$im = FRInclusionManager::singleton();
-		$im->setReviewedVersions( self::$inputTmps, self::$inputFiles );
-		foreach ( self::$rOutputFiles as $x => $triple ) {
+		$im->setReviewedVersions( self::$inputTemplates, self::$inputFiles );
+		foreach ( self::$reviewedOutputFiles as $x => $triple ) {
 			list($dbKey,$expTS,$expSha1) = $triple;
 			$title = Title::makeTitleSafe( NS_FILE, $dbKey );
 			list($actualTS,$actualSha1) = $im->getReviewedFileVersion( $title );
@@ -139,8 +143,8 @@ class FRInclusionManagerTest extends PHPUnit_Framework_TestCase {
 	public function testStableTemplateVersions() {
 		$im = FRInclusionManager::singleton();
 		$im->setReviewedVersions( array(), array() );
-		$im->setStableVersionCache( self::$inputTmps, self::$inputFiles );
-		foreach ( self::$sOutputTmps as $x => $triple ) {
+		$im->setStableVersionCache( self::$inputTemplates, self::$inputFiles );
+		foreach ( self::$stableOutputTemplates as $x => $triple ) {
 			list($ns,$dbKey,$expId) = $triple;
 			$title = Title::makeTitleSafe( $ns, $dbKey );
 			$actual = $im->getStableTemplateVersion( $title );
@@ -151,8 +155,8 @@ class FRInclusionManagerTest extends PHPUnit_Framework_TestCase {
 	public function testStableFileVersions() {
 		$im = FRInclusionManager::singleton();
 		$im->setReviewedVersions( array(), array() );
-		$im->setStableVersionCache( self::$inputTmps, self::$inputFiles );
-		foreach ( self::$sOutputFiles as $x => $triple ) {
+		$im->setStableVersionCache( self::$inputTemplates, self::$inputFiles );
+		foreach ( self::$stableOutputFiles as $x => $triple ) {
 			list($dbKey,$expTS,$expSha1) = $triple;
 			$title = Title::makeTitleSafe( NS_FILE, $dbKey );
 			list($actualTS,$actualSha1) = $im->getStableFileVersion( $title );
