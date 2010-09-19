c619a56876b59eca817e9eba6e7a549be787a0f3
Siebrand Mazeland <siebrand@users.mediawiki.org>
2010-09-19 09:01:11 +0000

    Revert r73306. Causes many notices.

--- language/FlaggedRevs.alias.php
+++ language/FlaggedRevs.alias.php
@@ -6,8 +6,6 @@
  * @ingroup Extensions
  */
 
-$specialPageAliases = array();
-
 /** English (English) */
 $specialPageAliases['en'] = array(
 	'PendingChanges' => array( 'PendingChanges', 'OldReviewedPages' /* deprecated */ ),
