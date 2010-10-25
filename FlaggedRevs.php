0baa436bd5b29f6981ca54d9f3b54dfa99a873fe
Rob Lanphier <robla@users.mediawiki.org>
2010-10-25 23:44:00 +0000

    Bumping $wgFlaggedRevStyleVersion

--- FlaggedRevs.php
+++ FlaggedRevs.php
@@ -255,7 +255,7 @@ $wgAvailableRights[] = 'movestable';
 $wgAvailableRights[] = 'stablesettings';
 
 # Bump this number every time you change flaggedrevs.css/flaggedrevs.js
-$wgFlaggedRevStyleVersion = '78';
+$wgFlaggedRevStyleVersion = '79';
 
 $wgExtensionFunctions[] = 'efLoadFlaggedRevs';
 
