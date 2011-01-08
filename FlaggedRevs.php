31991a412c63795202583fa18aa05e8c141ee81f
Aaron Schulz <aaron@users.mediawiki.org>
2011-01-08 19:59:04 +0000

    Bump for r79872

--- FlaggedRevs.php
+++ FlaggedRevs.php
@@ -246,7 +246,7 @@ $wgAvailableRights[] = 'movestable';
 $wgAvailableRights[] = 'stablesettings';
 
 # Bump this number every time you change flaggedrevs.css/flaggedrevs.js
-$wgFlaggedRevStyleVersion = 84;
+$wgFlaggedRevStyleVersion = 85;
 
 $wgExtensionFunctions[] = 'efLoadFlaggedRevs';
 
