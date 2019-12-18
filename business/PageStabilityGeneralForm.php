832493426f88ebc65f8c5ddeae861608d55965c1
Reedy <reedy@wikimedia.org>
2019-12-18 00:40:07 +0000

    Move PageStabilityGeneralForm into its own file
    Change-Id: Ie8a5a0e3f8f1446e2a32902da6dd4ba85b62388c

---
+++ business/PageStabilityGeneralForm.php
@@ -0,0 +1,42 @@
+<?php
+
+// Assumes $wgFlaggedRevsProtection is off
+class PageStabilityGeneralForm extends PageStabilityForm {
+	public function getReviewThis() {
+		return $this->reviewThis;
+	}
+
+	public function setReviewThis( $value ) {
+		$this->trySet( $this->reviewThis, $value );
+	}
+
+	public function getOverride() {
+		return $this->override;
+	}
+
+	public function setOverride( $value ) {
+		$this->trySet( $this->override, $value );
+	}
+
+	protected function reallyDoPreloadParameters() {
+		$oldConfig = $this->getOldConfig();
+		$this->override = $oldConfig['override'];
+		$this->autoreview = $oldConfig['autoreview'];
+		$this->watchThis = $this->getUser()->isWatched( $this->page );
+		return true;
+	}
+
+	protected function reallyDoCheckParameters() {
+		$this->override = $this->override ? 1 : 0; // default version settings is 0 or 1
+		// Check autoreview restriction setting
+		if ( $this->autoreview != '' // restriction other than 'none'
+			&& !in_array( $this->autoreview, FlaggedRevs::getRestrictionLevels() )
+		) {
+			return 'stabilize_invalid_autoreview'; // invalid value
+		}
+		if ( !FlaggedRevs::userCanSetAutoreviewLevel( $this->user, $this->autoreview ) ) {
+			return 'stabilize_denied'; // invalid value
+		}
+		return true;
+	}
+}
---
+++ business/PageStabilityProtectForm.php
@@ -0,0 +1,35 @@
+<?php
+
+// Assumes $wgFlaggedRevsProtection is on
+class PageStabilityProtectForm extends PageStabilityForm {
+	protected function reallyDoPreloadParameters() {
+		$oldConfig = $this->getOldConfig();
+		$this->autoreview = $oldConfig['autoreview']; // protect level
+		$this->watchThis = $this->getUser()->isWatched( $this->page );
+		return true;
+	}
+
+	protected function reallyDoCheckParameters() {
+		$oldConfig = $this->getOldConfig();
+		# Autoreview only when protecting currently unprotected pages
+		$this->reviewThis = ( FRPageConfig::getProtectionLevel( $oldConfig ) == 'none' );
+		# Autoreview restriction => use stable
+		# No autoreview restriction => site default
+		$this->override = ( $this->autoreview != '' )
+			? 1 // edits require review before being published
+			: (int)FlaggedRevs::isStableShownByDefault(); // site default
+		# Check that settings are a valid protection level...
+		$newConfig = [
+			'override'   => $this->override,
+			'autoreview' => $this->autoreview
+		];
+		if ( FRPageConfig::getProtectionLevel( $newConfig ) == 'invalid' ) {
+			return 'stabilize_invalid_level'; // double-check configuration
+		}
+		# Check autoreview restriction setting
+		if ( !FlaggedRevs::userCanSetAutoreviewLevel( $this->user, $this->autoreview ) ) {
+			return 'stabilize_denied'; // invalid value
+		}
+		return true;
+	}
+}
