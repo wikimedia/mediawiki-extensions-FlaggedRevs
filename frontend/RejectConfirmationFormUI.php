wikimedia/mediawiki-extensions-FlaggedRevs

commit 46291eda776f722ffe61b2a0904e11ea6cd5f014

Remove use of Revision::getTextId
Bug: T246284
Change-Id: I627bf2699fb7b2c1b2a53fb7b02a68c8390430dd

frontend/RejectConfirmationFormUI.php
@@ -1,6 +1,7 @@
 <?php
 
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Revision\RevisionRecord;
 
 /**
  * Reject confirmation review form UI
@@ -60,20 +61,27 @@ class RejectConfirmationFormUI {
 
 		$contribs = SpecialPage::getTitleFor( 'Contributions' )->getPrefixedText();
 
-		$lastTextId = 0;
+		$lastRevRecord = null;
 		$rejectIds = $rejectAuthors = [];
 		$lastRejectAuthor = null;
+		$revFactory = MediaWikiServices::getInstance()->getRevisionFactory();
 		foreach ( $res as $row ) {
-			$rev = new Revision( $row );
-			if ( $rev->getTextId() != $lastTextId ) { // skip null edits
-				$rejectIds[] = $rev->getId();
-				$rejectAuthors[] = $rev->isDeleted( Revision::DELETED_USER )
+			$revRecord = $revFactory->newRevisionFromRow( $row );
+
+			// skip null edits, check that $lastRevRecord isn't null because it starts
+			// null but null cannot be passed as the paramater to hasSameContent
+			if ( $lastRevRecord !== null && !$revRecord->hasSameContent( $lastRevRecord ) ) {
+				$rejectIds[] = $revRecord->getId();
+				$user = $revRecord->getUser();
+				$userText = $user ? $user->getName() : '';
+
+				$rejectAuthors[] = $revRecord->isDeleted( RevisionRecord::DELETED_USER )
 					? wfMessage( 'rev-deleted-user' )->text()
-					: "[[{$contribs}/{$rev->getUserText()}|{$rev->getUserText()}]]";
+					: "[[{$contribs}/{$userText}|{$userText}]]";
 				// Used for GENDER support for revreview-reject-summary-*
-				$lastRejectAuthor = $rev->getUserText();
+				$lastRejectAuthor = $userText;
 			}
-			$lastTextId = $rev->getTextId();
+			$lastRevRecord = $revRecord;
 		}
 		$rejectAuthors = array_values( array_unique( $rejectAuthors ) );
 

backend/FlaggedRevision.php
@@ -1,6 +1,7 @@
 <?php
 
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Revision\RevisionRecord;
 
 /**
  * Class representing a stable version of a MediaWiki revision
@@ -783,13 +784,14 @@ class FlaggedRevision {
 			return true; // later deleted
 		}
 		if ( $revIdDraft && $revIdUsed && $revIdDraft != $revIdUsed ) {
-			$dRev = Revision::newFromId( $revIdDraft );
-			$sRev = Revision::newFromId( $revIdUsed );
-			if ( !$sRev || $sRev->isDeleted( Revision::DELETED_TEXT ) ) {
+			$revLookup = MediaWikiServices::getInstance()->getRevisionLookup();
+			$sRevRecord = $revLookup->getRevisionById( $revIdUsed );
+			if ( !$sRevRecord || $sRevRecord->isDeleted( RevisionRecord::DELETED_TEXT ) ) {
 				return true; // rev deleted
 			}
+			$dRevRecord = $revLookup->getRevisionById( $revIdDraft );
 			# Don't do this for null edits (like protection) (bug 25919)
-			if ( $dRev && $dRev->getTextId() != $sRev->getTextId() ) {
+			if ( $dRevRecord && !$dRevRecord->hasSameContent( $sRevRecord ) ) {
 				return true; // updated
 			}
 		}

backend/FlaggedRevsHooks.php
@@ -740,8 +740,12 @@ class FlaggedRevsHooks {
 		# Is this a rollback/undo that didn't change anything?
 		if ( $baseId > 0 ) {
 			$frev = FlaggedRevision::newFromTitle( $title, $baseId ); // base rev of null edit
-			$pRev = Revision::newFromId( $rev->getParentId() ); // current rev parent
-			$revIsNull = ( $pRev && $pRev->getTextId() == $rev->getTextId() );
+			$pRevRecord = MediaWikiServices::getInstance()
+				->getRevisionLookup()
+				->getRevisionById( $rev->getParentId() ); // current rev parent
+			$revIsNull = ( $pRevRecord &&
+				$pRevRecord->hasSameContent( $rev->getRevisionRecord() )
+			);
 			# Was the edit that we tried to revert to reviewed?
 			# We avoid auto-reviewing null edits to avoid confusion (bug 28476).
 			if ( $frev && !$revIsNull ) {

commit a7250d01458a0e5cd8e66b5ef812a957ce27a6e2

Fix logic for determining if pending edits were null
Caused by 46291eda776f722ffe61b2a0904e11ea6cd5f014

Bug: T249277
Change-Id: Iec3ef9b45ee8268dd95d1ac4e1a023d1905ffc6a

frontend/RejectConfirmationFormUI.php
@@ -68,9 +68,10 @@ class RejectConfirmationFormUI {
 		foreach ( $res as $row ) {
 			$revRecord = $revFactory->newRevisionFromRow( $row );
 
-			// skip null edits, check that $lastRevRecord isn't null because it starts
-			// null but null cannot be passed as the paramater to hasSameContent
-			if ( $lastRevRecord !== null && !$revRecord->hasSameContent( $lastRevRecord ) ) {
+			// skip null edits; if $lastRevRecord is null then this is the first
+			// edit being checked, otherwise compare the content to the previous
+			// revision record
+			if ( $lastRevRecord === null || !$revRecord->hasSameContent( $lastRevRecord ) ) {
 				$rejectIds[] = $revRecord->getId();
 				$user = $revRecord->getUser();
 				$userText = $user ? $user->getName() : '';