05eeea394c3531000acacdd76b547b10d70fe227
daniel <dkinzler@wikimedia.org>
2020-10-13 23:14:13 +0200

    Add basic test for testParseStableRevisionPooled.
    The only thing this covers is really the code changed by I3ed1188e2.
    Still, it's a start.
    
    Change-Id: I916beec155c8d6797041ac7f351cbf11a6faef08

---
+++ tests/phpunit/FlaggedRevsBackendTest.php
@@ -0,0 +1,54 @@
+<?php
+
+use MediaWiki\Storage\RevisionRecord;
+
+/**
+ * @group Database
+ */
+class FlaggedRevsBackendTest extends MediaWikiIntegrationTestCase {
+
+	/**
+	 * @covers FlaggedRevs::parseStableRevisionPooled
+	 */
+	public function testParseStableRevisionPooled() {
+		$page = $this->getExistingTestPage( __METHOD__ );
+		$rev = $page->getRevisionRecord();
+		$title = $page->getTitle();
+		$user = $rev->getUser( RevisionRecord::RAW );
+		$comment = $rev->getComment( RevisionRecord::RAW );
+
+		$row = [
+			'rev_page' => $rev->getPageId(),
+			'rev_id' => $rev->getId(),
+			'rev_user' => $user->getId(),
+			'rev_actor' => $user->getActorId(),
+			'rev_minor_edit' => $rev->isMinor(),
+			'rev_deleted' => $rev->getVisibility(),
+			'rev_timestamp' => $rev->getTimestamp(),
+			'rev_parent_id' => $rev->getParentId(),
+			'rev_len' => $rev->getSize(),
+			'rev_sha1' => $rev->getSha1(),
+			'rev_comment_id' => $comment->id,
+			'rev_comment_text' => $comment->text,
+			'rev_comment_data' => $comment->data,
+			'page_namespace' => $title->getNamespace(),
+			'page_title' => $title->getDBkey(),
+			'page_latest' => $title->getLatestRevID(),
+			'fr_timestamp' => null,
+			'fr_quality' => null,
+			'fr_tags' => null,
+			'fr_flags' => null,
+			'fr_user' => $user->getId(),
+			'fr_img_name' => null,
+			'fr_img_sha1' => null,
+			'fr_img_timestamp' => null
+		];
+
+		$popts = ParserOptions::newFromAnon();
+		$frev = new FlaggedRevision( (object)$row, $title );
+
+		$out = FlaggedRevs::parseStableRevisionPooled( $frev, $popts );
+		$this->assertNotNull( $out );
+	}
+
+}
