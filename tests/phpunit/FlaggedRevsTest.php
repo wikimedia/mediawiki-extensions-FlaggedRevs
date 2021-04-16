59aee2001fcce71406582fd400e647296e454106
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2021-04-16 10:45:54 +0200

    Make FlaggedRevsTest more robust, part 2
    This test fails in the BlueSpiceFlaggedRevsConnector extension.
    I think I understand why:
    
    The BlueSpice extension contains a large wgExtensionFunctions
    function that manipulates the global that is relevant for this
    test. While the test tries to reset the global, this doesn't
    have an effect. Some unrelated hook initialized the FlaggedRevs
    class already.
    
    The getLevels() feature also depends on another config which
    should be forced to  match what the test expects.
    
    Change-Id: I070f97eb199ac421f7f76d4d600c65354fffb199

--- tests/phpunit/FlaggedRevsTest.php
+++ tests/phpunit/FlaggedRevsTest.php
@@ -1,13 +1,20 @@
 <?php
 
+use Wikimedia\TestingAccessWrapper;
+
 /**
  * @covers \FlaggedRevs
  */
 class FlaggedRevsTest extends MediaWikiIntegrationTestCase {
 
 	public function testGetLevels() {
+		// Force reloading from the modified globals below
+		$flaggedRevs = TestingAccessWrapper::newFromClass( FlaggedRevs::class );
+		$flaggedRevs->loaded = false;
+
 		$this->setMwGlobals( [
 			'wgExtensionFunctions' => [],
+			'wgFlaggedRevsProtection' => false,
 			'wgFlaggedRevsTags' => [ 'accuracy' => [ 'levels' => 3, 'quality' => 2 ] ],
 		] );
 
