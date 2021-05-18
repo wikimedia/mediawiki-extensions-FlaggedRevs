17e1044ff01a75c06efeab62136c4bf67ec82707
Roman Stolar <roman.stolar@speedandfunction.com>
2021-05-18 17:14:50 +0300

    Create diffheader endpoint through a RESTful interface
    Bug: T94006
    Change-Id: I02417eed55c4fc7a9710c4a57b9e20a68a54b2c8

---
+++ rest/DiffHeaderHandler.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace MediaWiki\Extension\FlaggedRevs\Rest;
+
+use FlaggablePageView;
+use MediaWiki\Rest\Response;
+use MediaWiki\Rest\SimpleHandler;
+use MediaWiki\Rest\StringStream;
+use Wikimedia\ParamValidator\ParamValidator;
+
+/**
+ *
+ * Handler class for REST API endpoints that update diff header items
+ */
+class DiffHeaderHandler extends SimpleHandler {
+
+	/**
+	 * @param int $oldId
+	 * @param int $newId
+	 * @return Response
+	 */
+	public function run( int $oldId, int $newId ) {
+		$html = FlaggablePageView::AjaxBuildDiffHeaderItems( $oldId, $newId );
+		$response = $this->getResponseFactory()->create();
+		$response->setBody( new StringStream( $html ) );
+		$response->setHeader( 'Content-Type', 'text/html' );
+		return $response;
+	}
+
+	public function needsWriteAccess() {
+		return false;
+	}
+
+	/** @inheritDoc */
+	public function getParamSettings() {
+		return [
+			'oldId' => [
+				self::PARAM_SOURCE => 'path',
+				ParamValidator::PARAM_REQUIRED => true,
+				ParamValidator::PARAM_TYPE => 'integer',
+			],
+			'newId' => [
+				self::PARAM_SOURCE => 'path',
+				ParamValidator::PARAM_REQUIRED => true,
+				ParamValidator::PARAM_TYPE => 'integer',
+			]
+		];
+	}
+}
