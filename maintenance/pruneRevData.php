45ae13c245c3d9b37d7a0d7e9da81d3b4e95d4a3
Amir Sarabadani <ladsgroup@gmail.com>
2021-11-22 06:05:10 +0100

    Major fixes to maintenance/pruneRevData.php
     - Fix documentation, it's not deleting older than a month, it's 1 hour
     - Avoid using $db->freeResult(), php has a garbage collector
     - Move reads to replica, there is no danger in getting out-dated
       results and it puts a lot of pressure on the primary database.
     - Split deletes to batch size configured in the script not 5000 together.
       This is where batching actually matters and causes replication lag.
     - Avoid using the word "sanity"
     - Avoid using seek() and just use selectFieldValues. It's 50 integers,
       it fits in the memory
     - Wait for replication when it's actually deleting
     - Remove "replica aware" variable. They serve no purpose.
     - Flip the condition to avoid too much nesting
     - Don't reset number of deleted rows half-way through the run
     - Remove unused variables.
    
    Bug: T290769
    Change-Id: I249f9c0d25c4bd22e0281de805baa359c98eed81
    (cherry picked from commit 6e84ef45c3f6d8a804fe2a041a73ac1c8414693f)

--- maintenance/pruneRevData.php
+++ maintenance/pruneRevData.php
@@ -18,7 +18,7 @@ class PruneFRIncludeData extends Maintenance {
 	public function __construct() {
 		parent::__construct();
 		$this->addDescription( "This script clears template data for reviewed versions" .
-			"that are 1+ month old and have 50+ newer versions in page. By default," .
+			"that are 1+ hour old and have 50+ newer versions in page. By default," .
 			"it will just output how many rows can be deleted. Use the 'prune' option " .
 			"to actually delete them."
 		);
@@ -49,12 +49,13 @@ class PruneFRIncludeData extends Maintenance {
 			$this->output( "Running dry-run of old flagged revision inclusion data pruning...\n" );
 		}
 
-		$db = wfGetDB( DB_PRIMARY );
+		$dbw = wfGetDB( DB_PRIMARY );
+		$dbr = wfGetDB( DB_REPLICA );
 
 		if ( $start === null ) {
-			$start = $db->selectField( 'flaggedpages', 'MIN(fp_page_id)', false, __METHOD__ );
+			$start = $dbr->selectField( 'flaggedpages', 'MIN(fp_page_id)', false, __METHOD__ );
 		}
-		$end = $db->selectField( 'flaggedpages', 'MAX(fp_page_id)', false, __METHOD__ );
+		$end = $dbr->selectField( 'flaggedpages', 'MAX(fp_page_id)', false, __METHOD__ );
 		if ( $start === null || $end === null ) {
 			$this->output( "...flaggedpages table seems to be empty.\n" );
 			return;
@@ -63,88 +64,71 @@ class PruneFRIncludeData extends Maintenance {
 		$blockStart = (int)$start;
 		$blockEnd = (int)$start + $this->mBatchSize - 1;
 
-		// Tallies
+		// Tally
 		$tDeleted = 0;
-		$fDeleted = 0;
 
 		$newerRevs = 50;
 		$sleep = (int)$this->getOption( 'sleep', 0 );
-		$cutoff = $db->timestamp( time() - 3600 );
+		$cutoff = $dbr->timestamp( time() - 3600 );
 
 		$lbFactory = MediaWikiServices::getInstance()->getDBLoadBalancerFactory();
 
 		while ( $blockEnd <= $end ) {
 			$this->output( "...doing fp_page_id from $blockStart to $blockEnd\n" );
 			$cond = "fp_page_id BETWEEN $blockStart AND $blockEnd";
-			$res = $db->select( 'flaggedpages', 'fp_page_id', $cond, __METHOD__ );
-			$batchCount = 0; // rows deleted without replica lag check
+			$res = $dbr->select( 'flaggedpages', 'fp_page_id', $cond, __METHOD__ );
 			// Go through a chunk of flagged pages...
 			foreach ( $res as $row ) {
 				// Get the newest X ($newerRevs) flagged revs for this page
-				$sres = $db->select( 'flaggedrevs',
+				$sres = $dbr->selectFieldValues( 'flaggedrevs',
 					'fr_rev_id',
 					[ 'fr_page_id' => $row->fp_page_id ],
 					__METHOD__,
 					[ 'ORDER BY' => 'fr_rev_id DESC', 'LIMIT' => $newerRevs ]
 				);
 				// See if there are older revs that can be pruned...
-				if ( $db->numRows( $sres ) == $newerRevs ) {
-					// Get the oldest of the top X revisions
-					$sres->seek( $newerRevs - 1 );
-					$lrow = $db->fetchObject( $sres );
-					$oldestId = (int)$lrow->fr_rev_id; // oldest revision Id
-					// Get revs not in the top X that were not reviewed recently
-					$db->freeResult( $sres );
-					$sres = $db->select( 'flaggedrevs',
-						'fr_rev_id',
-						[
-							'fr_page_id' => $row->fp_page_id,
-							'fr_rev_id < ' . $oldestId, // not in the newest X
-							'fr_timestamp < ' . $db->addQuotes( $cutoff ) // not reviewed recently
-						],
-						__METHOD__,
-						// Sanity check (start with the oldest)
-						[ 'ORDER BY' => 'fr_rev_id ASC', 'LIMIT' => 5000 ]
-					);
-					// Build an array of these rev Ids
-					$revsClearIncludes = [];
-					foreach ( $sres as $srow ) {
-						$revsClearIncludes[] = $srow->fr_rev_id;
-					}
-					$batchCount += count( $revsClearIncludes ); // # of revs to prune
-					$db->freeResult( $sres );
-					if ( !$revsClearIncludes ) {
-						// @phan-suppress-previous-line PhanPluginRedundantAssignmentInLoop
-						$tDeleted = 0;
-						$fDeleted = 0;
-					} elseif ( $prune ) {
-						// Write run: clear the include data for these old revs
-						$db->begin( __METHOD__ );
-						$db->delete( 'flaggedtemplates',
-							[ 'ft_rev_id' => $revsClearIncludes ],
+				if ( count( $sres ) !== $newerRevs ) {
+					continue;
+				}
+				// Get the oldest of the top X revisions
+				$oldestId = (int)$sres[ $newerRevs - 1 ];
+				// Get revs not in the top X that were not reviewed recently
+				$sres = $dbr->select( 'flaggedrevs',
+					'fr_rev_id',
+					[
+						'fr_page_id' => $row->fp_page_id,
+						'fr_rev_id < ' . $oldestId, // not in the newest X
+						'fr_timestamp < ' . $dbr->addQuotes( $cutoff ) // not reviewed recently
+					],
+					__METHOD__,
+					[ 'ORDER BY' => 'fr_rev_id ASC', 'LIMIT' => 5000 ]
+				);
+				// Build an array of these rev Ids
+				$revsClearIncludes = [];
+				foreach ( $sres as $srow ) {
+					$revsClearIncludes[] = $srow->fr_rev_id;
+				}
+				if ( !$revsClearIncludes ) {
+					continue;
+				} elseif ( $prune ) {
+					foreach ( array_chunk( $revsClearIncludes, $this->mBatchSize ) as $batch ) {
+						$dbw->delete( 'flaggedtemplates',
+							[ 'ft_rev_id' => $batch ],
 							__METHOD__
 						);
-						$tDeleted += $db->affectedRows();
-						$db->commit( __METHOD__ );
-					} else {
-						// Dry run: say how many includes rows would have been cleared
-						$tDeleted += $db->selectField( 'flaggedtemplates',
-							'COUNT(*)',
-							[ 'ft_rev_id' => $revsClearIncludes ],
-							__METHOD__
-						);
-					}
-					// Check replica lag...
-					if ( $batchCount >= $this->mBatchSize ) {
-						$batchCount = 0;
-						$lbFactory->waitForReplication( [ 'ifWritesSince' => 5 ] );
+						$tDeleted += $dbw->affectedRows();
+						$lbFactory->waitForReplication();
 						sleep( $sleep );
 					}
 				} else {
-					$db->freeResult( $sres );
+					// Dry run: say how many includes rows would have been cleared
+					$tDeleted += $dbr->selectField( 'flaggedtemplates',
+						'COUNT(*)',
+						[ 'ft_rev_id' => $revsClearIncludes ],
+						__METHOD__
+					);
 				}
 			}
-			$db->freeResult( $res );
 			$blockStart += $this->mBatchSize;
 			$blockEnd += $this->mBatchSize;
 		}
