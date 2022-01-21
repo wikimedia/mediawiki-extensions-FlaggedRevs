5a3027abc421a8569f459b2cfd86805f0c30bf10
Timo Tijhof <krinkle@fastmail.com>
2022-01-21 04:15:17 +0000

    ext.flaggedRevs.review: Update to current coding conventions
    * Use dollar sign in variables holding jQuery collections.
      Not just for clarity, but also to enable relevant ESLint rules
      to correctly detect other violations, which I then fixed.
    
    * Move some variable declarations inline for clarity per the
      new ruleset.
    
    * Remove various confusing, inaccurate, or overly verbose inline
      comments that were added in I9d0f7b880a1d96e.
    
    Change-Id: Ieaa31e9499928fc3ba5b79c28248c9b8e5e96010

--- modules/ext.flaggedRevs.review/review.js
+++ modules/ext.flaggedRevs.review/review.js
@@ -12,18 +12,18 @@
 	/*
 	 * Update <select> color for the selected item/option
 	 */
-	function updateReviewFormColors( form ) {
-		var tag, select, selectedlevel, value;
-		for ( tag in wgFlaggedRevsParams.tags ) { // for each tag
-			select = form.find( '[name="wp' + tag + '"]' ).eq( 0 );
+	function updateReviewFormColors( $form ) {
+		var $select, selectedlevel, value;
+		for ( var tag in wgFlaggedRevsParams.tags ) { // for each tag
+			$select = $form.find( '[name="wp' + tag + '"]' ).eq( 0 );
 			// Look for a selector for this tag
-			if ( select.length && select.prop( 'nodeName' ) === 'SELECT' ) {
-				selectedlevel = select.prop( 'selectedIndex' );
-				value = select.children( 'option' ).eq( selectedlevel ).val();
-				select.prop( 'className', 'fr-rating-option-' + value );
+			if ( $select.length && $select.prop( 'nodeName' ) === 'SELECT' ) {
+				selectedlevel = $select.prop( 'selectedIndex' );
+				value = $select.children( 'option' ).eq( selectedlevel ).val();
+				$select.prop( 'className', 'fr-rating-option-' + value );
 				// Fix FF one-time jitter bug of changing an <option>
-				select.prop( 'selectedIndex', null );
-				select.prop( 'selectedIndex', selectedlevel );
+				$select.prop( 'selectedIndex', null );
+				$select.prop( 'selectedIndex', selectedlevel );
 			}
 		}
 	}
@@ -36,32 +36,32 @@
 	 * - Also remove comment box clutter in case of invalid input.
 	 * NOTE: all buttons should exist (perhaps hidden though)
 	 */
-	function updateReviewForm( form ) {
-		var somezero, tag, tagLevelSelects, tagLevelSelect, selectedlevel, i, l;
-		if ( form.prop( 'disabled' ) ) {
+	function updateReviewForm( $form ) {
+		if ( $form.prop( 'disabled' ) ) {
 			return;
 		}
 
-		somezero = false;
+		var somezero = false;
+		var $tagLevelSelects, $tagLevelSelect, selectedlevel;
 		// Determine if this is a "quality" or "incomplete" review
-		for ( tag in wgFlaggedRevsParams.tags ) {
+		for ( var tag in wgFlaggedRevsParams.tags ) {
 			// Get the element or elements for selecting the tag level.
 			// We might get back a select, a checkbox, or *several* radios.
-			tagLevelSelects = form.find( '[name="wp' + tag + '"]' );
-			if ( !tagLevelSelects.length ) {
+			$tagLevelSelects = $form.find( '[name="wp' + tag + '"]' );
+			if ( !$tagLevelSelects.length ) {
 				continue; // none found; binary flagging?
 			}
-			tagLevelSelect = tagLevelSelects.eq( 0 ); // convenient for select and checkbox
+			$tagLevelSelect = $tagLevelSelects.eq( 0 ); // convenient for select and checkbox
 
 			selectedlevel = 0; // default
-			if ( tagLevelSelect.prop( 'nodeName' ) === 'SELECT' ) {
-				selectedlevel = tagLevelSelect.prop( 'selectedIndex' );
-			} else if ( tagLevelSelect.prop( 'type' ) === 'checkbox' ) {
-				selectedlevel = tagLevelSelect.prop( 'checked' ) ? 1 : 0;
-			} else if ( tagLevelSelect.prop( 'type' ) === 'radio' ) {
+			if ( $tagLevelSelect.prop( 'nodeName' ) === 'SELECT' ) {
+				selectedlevel = $tagLevelSelect.prop( 'selectedIndex' );
+			} else if ( $tagLevelSelect.prop( 'type' ) === 'checkbox' ) {
+				selectedlevel = $tagLevelSelect.prop( 'checked' ) ? 1 : 0;
+			} else if ( $tagLevelSelect.prop( 'type' ) === 'radio' ) {
 				// Go through each radio option and find the selected one...
-				for ( i = 0, l = tagLevelSelects.length; i < l; i++ ) {
-					if ( tagLevelSelects.eq( i ).prop( 'checked' ) ) {
+				for ( var i = 0; i < $tagLevelSelects.length; i++ ) {
+					if ( $tagLevelSelects.eq( i ).prop( 'checked' ) ) {
 						selectedlevel = i;
 						break;
 					}
@@ -82,49 +82,45 @@
 			.val( mw.msg( 'revreview-submit-review' ) ); // reset to "Accept"
 
 		// Update colors of <select>
-		updateReviewFormColors( form );
+		updateReviewFormColors( $form );
 	}
 
 	/*
 	 * Lock review form from submissions (using during AJAX requests)
 	 */
-	function lockReviewForm( form ) {
-		form.find( 'input, textarea, select' ).prop( 'disabled', true );
+	function lockReviewForm( $form ) {
+		$form.find( 'input, textarea, select' ).prop( 'disabled', true );
 	}
 
 	/*
 	 * Unlock review form from submissions (using after AJAX requests)
 	 */
-	function unlockReviewForm( form ) {
-		var i, inputs = form.find( 'input' );
-		for ( i = 0; i < inputs.length; i++ ) {
-			if ( inputs.eq( i ).prop( 'type' ) !== 'submit' ) { // not all buttons can be enabled
-				inputs.eq( i ).prop( 'disabled', false );
+	function unlockReviewForm( $form ) {
+		var $inputs = $form.find( 'input' );
+		for ( var i = 0; i < $inputs.length; i++ ) {
+			if ( $inputs.eq( i ).prop( 'type' ) !== 'submit' ) { // not all buttons can be enabled
+				$inputs.eq( i ).prop( 'disabled', false );
 			} else {
-				inputs.eq( i ).blur(); // focus off element (bug 24013)
+				// focus off element (T26013)
+				$inputs.eq( i ).trigger( 'blur' );
 			}
 		}
-		form.find( 'textarea, select' ).prop( 'disabled', false );
+		$form.find( 'textarea, select' ).prop( 'disabled', false );
 	}
 
 	/*
 	 * Update form elements after review.
 	 */
-	function postSubmitRevisionReview( form, response ) {
-		var $asubmit, $usubmit, $rsubmit, $diffNotice,
-			$tagBox, $diffUIParams, oldId, newId, restPath, respObj;
-
-		respObj = response;
-
+	function postSubmitRevisionReview( $form, respObj ) {
 		// Review form elements
-		$asubmit = $( '#mw-fr-submit-accept' ); // ACCEPT
-		$usubmit = $( '#mw-fr-submit-unaccept' ); // UNACCEPT
-		$rsubmit = $( '#mw-fr-submit-reject' ); // REJECT
-		$diffNotice = $( '#mw-fr-difftostable' );
+		var $asubmit = $( '#mw-fr-submit-accept' ); // ACCEPT
+		var $usubmit = $( '#mw-fr-submit-unaccept' ); // UNACCEPT
+		var $rsubmit = $( '#mw-fr-submit-reject' ); // REJECT
+		var $diffNotice = $( '#mw-fr-difftostable' );
 		// FlaggedRevs rating box
-		$tagBox = $( '#mw-fr-revisiontag' );
+		var $tagBox = $( '#mw-fr-revisiontag' );
 		// Diff parameters
-		$diffUIParams = $( '#mw-fr-diff-dataform' );
+		var $diffUIParams = $( '#mw-fr-diff-dataform' );
 
 		// On success... (change-time can be an empty string for 'unapproved')
 		if ( Object.prototype.hasOwnProperty.call( respObj, 'change-time' ) ) {
@@ -158,19 +154,17 @@
 				// Hide "review this" box on diffs
 				$diffNotice.hide();
 				// Update the contents of the mw-fr-diff-headeritems div
-				oldId = $diffUIParams.find( 'input' ).eq( 0 ).val();
-				newId = $diffUIParams.find( 'input' ).eq( 1 ).val();
+				var oldId = $diffUIParams.find( 'input' ).eq( 0 ).val();
+				var newId = $diffUIParams.find( 'input' ).eq( 1 ).val();
 
-				// Send encoded function plus all arguments...
-				restPath = '/flaggedrevs/internal/diffheader/' +
+				var restPath = '/flaggedrevs/internal/diffheader/' +
 					encodeURIComponent( oldId ) + '/' +
 					encodeURIComponent( newId );
-
-				// Send GET request via AJAX!
 				$.ajax( {
 					url: mw.util.wikiScript( 'rest' ) + restPath,
 					type: 'GET',
-					dataType: 'html', // response type
+					// response type
+					dataType: 'html',
 					success: function ( html ) {
 						// Update the contents of the mw-fr-diff-headeritems div
 						$( '#mw-fr-diff-headeritems' ).html( html );
@@ -198,7 +192,7 @@
 		if ( Object.prototype.hasOwnProperty.call( respObj, 'change-time' ) ) {
 			$( '#mw-fr-input-changetime' ).val( respObj[ 'change-time' ] );
 		}
-		unlockReviewForm( form );
+		unlockReviewForm( $form );
 	}
 
 	/*
@@ -207,60 +201,60 @@
 	 * Note: requestArgs build-up from radios/checkboxes
 	 * based on patch by Daniel Arnold (bug 13744)
 	 */
-	function submitRevisionReview( button, form ) {
-		var i, l, postData, inputs, input, selects, select,
-			soption, restPath, target;
-		lockReviewForm( form ); // disallow submissions
-		// Build up arguments array and update submit button text...
-		postData = {}; // dictionary of the form: {key: value}
-		inputs = form.find( 'input' );
-		for ( i = 0; i < inputs.length; i++ ) {
-			input = inputs.eq( i );
-			if ( input.prop( 'name' ) === 'target' ) {
-				target = input.val();
+	function submitRevisionReview( button, $form ) {
+		lockReviewForm( $form ); // disallow submissions
+
+		// Build up API call, and update submit button text...
+		var postData = {};
+		var $inputs = $form.find( 'input' );
+		var target;
+		for ( var i = 0; i < $inputs.length; i++ ) {
+			var $input = $inputs.eq( i );
+			if ( $input.prop( 'name' ) === 'target' ) {
+				target = $input.val();
 			}
 			// Different input types may occur depending on tags...
-			if ( input.prop( 'name' ) === 'title' || input.prop( 'name' ) === 'action' ) {
+			if ( $input.prop( 'name' ) === 'title' || $input.prop( 'name' ) === 'action' ) {
 				continue; // No need to send these...
-			} else if ( input.prop( 'type' ) === 'submit' ) {
-				if ( input.prop( 'id' ) === button.id ) {
-					postData[ input.prop( 'name' ) ] = '1';
+			} else if ( $input.prop( 'type' ) === 'submit' ) {
+				if ( $input.prop( 'id' ) === button.id ) {
+					postData[ $input.prop( 'name' ) ] = '1';
 					// Show that we are submitting via this button
-					input.val( mw.msg( 'revreview-submitting' ) );
+					$input.val( mw.msg( 'revreview-submitting' ) );
 				}
-			} else if ( input.prop( 'type' ) === 'checkbox' ) {
-				postData[ input.prop( 'name' ) ] = input.prop( 'checked' ) ? input.val() : 0;
-			} else if ( input.prop( 'type' ) === 'radio' ) {
-				if ( input.prop( 'checked' ) ) { // must be checked
-					postData[ input.prop( 'name' ) ] = input.val();
+			} else if ( $input.prop( 'type' ) === 'checkbox' ) {
+				postData[ $input.prop( 'name' ) ] = $input.prop( 'checked' ) ? $input.val() : 0;
+			} else if ( $input.prop( 'type' ) === 'radio' ) {
+				if ( $input.prop( 'checked' ) ) { // must be checked
+					postData[ $input.prop( 'name' ) ] = $input.val();
 				}
 			} else {
-				postData[ input.prop( 'name' ) ] = input.val(); // text/hiddens...
+				postData[ $input.prop( 'name' ) ] = $input.val(); // text/hiddens...
 			}
 		}
-		selects = form.find( 'select' );
-		for ( i = 0, l = selects.length; i < l; i++ ) {
-			select = selects.eq( i );
+		var $selects = $form.find( 'select' );
+		for ( var j = 0; j < $selects.length; j++ ) {
+			var $select = $selects.eq( j );
 			// Get the selected tag level...
-			if ( select.prop( 'selectedIndex' ) >= 0 ) {
-				soption = select.find( 'option' ).eq( select.prop( 'selectedIndex' ) );
-				postData[ select.prop( 'name' ) ] = soption.val();
+			if ( $select.prop( 'selectedIndex' ) >= 0 ) {
+				var $soption = $select.find( 'option' ).eq( $select.prop( 'selectedIndex' ) );
+				postData[ $select.prop( 'name' ) ] = $soption.val();
 			}
 		}
 
-		restPath = '/flaggedrevs/internal/review/' + encodeURIComponent( target );
-		// Send POST request via AJAX!
+		var restPath = '/flaggedrevs/internal/review/' + encodeURIComponent( target );
 		$.ajax( {
 			url: mw.util.wikiScript( 'rest' ) + restPath,
 			type: 'POST',
 			data: JSON.stringify( postData ),
 			contentType: 'application/json',
-			dataType: 'json', // response type
+			// response type
+			dataType: 'json',
 			success: function ( response ) {
-				postSubmitRevisionReview( form, response );
+				postSubmitRevisionReview( $form, response );
 			},
 			error: function ( response ) {
-				postSubmitRevisionReview( form, response.responseJSON );
+				postSubmitRevisionReview( $form, response.responseJSON );
 			}
 		} );
 	}
