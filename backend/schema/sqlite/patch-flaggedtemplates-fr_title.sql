26132be2eed9250fffe96ed01d2671ea80c91ba1
Amir Sarabadani <ladsgroup@gmail.com>
2022-01-28 19:29:28 +0100

    Fix alter table for sqlite
    It needs to rename the table once done
    
    Change-Id: I2740ff117750078b83181055557c6bc2c5a2b93a

--- backend/schema/sqlite/patch-flaggedtemplates-fr_title.sql
+++ backend/schema/sqlite/patch-flaggedtemplates-fr_title.sql
@@ -10,3 +10,5 @@ INSERT OR IGNORE INTO /*_*/flaggedtemplates_tmp (ft_rev_id, ft_tmp_rev_id)
   FROM /*_*/flaggedtemplates;
 
 DROP TABLE /*_*/flaggedtemplates;
+
+ALTER TABLE /*_*/flaggedtemplates_tmp RENAME TO /*_*/flaggedtemplates;
