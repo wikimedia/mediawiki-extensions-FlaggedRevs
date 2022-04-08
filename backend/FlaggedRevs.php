3238dbec9a88b9e9807bb8d80632455b63dd9818
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2022-04-08 18:46:22 +0200

    Show correct error message when PoolCounter is full
    Before is was possible to see an "article missing" error just because
    the pool counter was full. This is to show the correct error message.
    
    See T305725 for a longer explanation.
    
    Bug: T305725
    Change-Id: I354708a9526d3a4f7a9784e71bed0a79f4d44060

--- backend/FlaggedRevs.php
+++ backend/FlaggedRevs.php
@@ -117,7 +117,6 @@ class FlaggedRevs {
 	}
 
 	/**
-	 * Return the include handling configuration
 	 * @return int
 	 */
 	public static function inclusionSetting() {
@@ -310,6 +309,7 @@ class FlaggedRevs {
 		FlaggedRevision $frev, ParserOptions $pOpts
 	) {
 		$page = WikiPage::factory( $frev->getTitle() );
+		/** @var FlaggedRevsParserCache $stableParserCache */
 		$stableParserCache = MediaWikiServices::getInstance()->getService( 'FlaggedRevsParserCache' );
 		$keyPrefix = $stableParserCache->makeKey( $page, $pOpts );
 
@@ -326,7 +326,10 @@ class FlaggedRevs {
 				},
 				'fallback' => static function () use ( $page, $pOpts, $stableParserCache ) {
 					// Use stale cache if possible
-					return Status::newGood( $stableParserCache->getDirty( $page, $pOpts ) ?: null );
+					$parserOutput = $stableParserCache->getDirty( $page, $pOpts );
+					// The fallback wasn't able to prevent the error situation, return false to
+					// continue the original error handling
+					return $parserOutput ? Status::newGood( $parserOutput ) : false;
 				},
 				'error' => static function ( Status $status ) {
 					return $status;
