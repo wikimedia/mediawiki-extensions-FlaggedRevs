89fb75665763e0655f6dc1ad4810e52c5615887e
Martin Urbanec <martin@urbanec.cz>
2022-10-22 22:02:33 +0200

    SpecialContributions: Make it possible to see pending changes only
    When $wgFlaggedRevsProtection is false (most flaggedrevs-enabled
    wikis), this table is already joined for Special:Contributions
    queries, so this should not have a significant effect
    performance-wise. The filter is disabled on
    $wgFlaggedRevsProtection wikis for now.
    
    This will be helpful for GrowthExperiments, as we want
    to allow mentors see pending edits by their mentees
    (by linking to a filtered view of Special:Contributions).
    
    Bug: T321445
    Change-Id: Ibbb5e3b70280fa12f7084aca0dba378e386e1f35

---
+++ backend/FlaggedRevsContributionsHooks.php
@@ -0,0 +1,46 @@
+<?php
+// phpcs:disable MediaWiki.NamingConventions.LowerCamelFunctionsName.FunctionName
+
+namespace Mediawiki\Extension\FlaggedRevs\Backend;
+
+use MediaWiki\Hook\ContribsPager__getQueryInfoHook;
+use MediaWiki\Hook\SpecialContributions__getForm__filtersHook;
+
+class FlaggedRevsContributionsHooks implements
+	SpecialContributions__getForm__filtersHook,
+	ContribsPager__getQueryInfoHook
+{
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onSpecialContributions__getForm__filters( $sp, &$filters ) {
+		$filters[] = [
+			'type' => 'check',
+			'label-message' => 'flaggedrevs-contributions-filters-unreviewed-only',
+			'name' => 'flaggedrevs-only-pending',
+		];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onContribsPager__getQueryInfo( $pager, &$queryInfo ) {
+		if ( $pager->getConfig()->get( 'FlaggedRevsProtection' ) ) {
+			return;
+		}
+
+		if ( $pager->getContext()->getRequest()->getBool( 'flaggedrevs-only-pending' ) ) {
+			// only add the flaggedpages table if not already done by
+			// FlaggedRevsUIHooks::addToContribsQuery
+			if ( !in_array( 'flaggedrevs', $queryInfo['tables'] ) ) {
+				$queryInfo['tables'][] = 'flaggedpages';
+				$queryInfo['join_conds']['flaggedpages'] = [ 'LEFT JOIN', "fp_page_id = rev_page" ];
+			}
+
+			// filter down to pending changes only
+			$queryInfo['conds'][] = 'fp_stable < rev_id';
+			$queryInfo['conds'][] = 'fp_pending_since IS NOT NULL';
+		}
+	}
+}
