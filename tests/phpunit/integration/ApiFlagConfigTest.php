dff98fdaebd23122d8dca19b8115d6d5a6c0c553
Novem Linguae <novemlinguae@gmail.com>
2023-12-30 02:34:05 -0800

    Add ApiFlagConfigTest
    Change-Id: Iebfee40ebf733c6e09f38c31ef658cef2c354672

---
+++ tests/phpunit/integration/ApiFlagConfigTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace MediaWiki\Extension\FlaggedRevs\Test;
+
+use ApiTestCase;
+use MediaWiki\Config\ConfigException;
+
+/**
+ * Tests for ApiFlagConfig class
+ *
+ * @covers \ApiFlagConfig
+ * @covers \FlaggedRevs::useOnlyIfProtected
+ * @covers \FlaggedRevs::getTagName
+ * @covers \FlaggedRevs::getMaxLevel
+ */
+class ApiFlagConfigTest extends ApiTestCase {
+	public function testFlagConfigFlaggedRevsProtection() {
+		$this->overrideConfigValue( 'FlaggedRevsProtection', true );
+		$params = [ 'action' => 'flagconfig' ];
+		[ $result ] = $this->doApiRequest( $params );
+		$expected = [ 'flagconfig' => [] ];
+		$this->assertEquals( $expected, $result );
+	}
+
+	public function testFlagConfigNoFlaggedRevsProtectionDefault() {
+		$this->overrideConfigValue( 'FlaggedRevsProtection', false );
+		$params = [ 'action' => 'flagconfig' ];
+		[ $result ] = $this->doApiRequest( $params );
+		$expected = [
+			'flagconfig' => [
+				[
+					'name' => 'accuracy',
+					'levels' => 3,
+					'tier1' => 1,
+				]
+			],
+		];
+		$this->assertEquals( $expected, $result );
+	}
+
+	public function testFlagConfigNoFlaggedRevsProtectionCustom() {
+		$this->overrideConfigValue( 'FlaggedRevsProtection', false );
+		$this->overrideConfigValue( 'FlaggedRevsTags', [
+			'accuracy' => [
+				'levels' => 2
+			]
+		] );
+		$params = [ 'action' => 'flagconfig' ];
+		[ $result ] = $this->doApiRequest( $params );
+		$expected = [
+			'flagconfig' => [
+				[
+					'name' => 'accuracy',
+					'levels' => 2,
+					'tier1' => 1,
+				]
+			],
+		];
+		$this->assertEquals( $expected, $result );
+	}
+
+	public function testFlagConfigNoFlaggedRevsProtectionError() {
+		$this->overrideConfigValue( 'FlaggedRevsProtection', false );
+		$this->overrideConfigValue( 'FlaggedRevsTags', [
+			'accuracy' => [
+				'levels' => 3
+			],
+			'tone' => [
+				'levels' => 3
+			]
+		] );
+		$params = [ 'action' => 'flagconfig' ];
+		$this->expectException( ConfigException::class );
+		$this->doApiRequest( $params );
+	}
+}
