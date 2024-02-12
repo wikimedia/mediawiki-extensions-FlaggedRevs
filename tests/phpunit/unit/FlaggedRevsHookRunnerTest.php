be6cc45409b11265fb9793c569cd221360bf1cb6
Umherirrender <umherirrender_de.wp@web.de>
2024-02-12 20:31:01 +0100

    Rename HookRunnerTest
    Unify the name accross the extensions
    
    Bug: T357339
    Change-Id: I539fcd9f64ed96f079cd19ad70014de552930349

--- tests/phpunit/unit/FlaggedRevsHookRunnerTest.php
+++ tests/phpunit/unit/FlaggedRevsHookRunnerTest.php
@@ -5,7 +5,7 @@ use MediaWiki\Tests\HookContainer\HookRunnerTestBase;
 /**
  * @covers \FlaggedRevsHookRunner
  */
-class HookRunnerTest extends HookRunnerTestBase {
+class FlaggedRevsHookRunnerTest extends HookRunnerTestBase {
 
 	public static function provideHookRunners() {
 		yield FlaggedRevsHookRunner::class => [ FlaggedRevsHookRunner::class ];
