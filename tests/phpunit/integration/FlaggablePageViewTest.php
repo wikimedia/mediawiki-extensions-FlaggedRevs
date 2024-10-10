027b4e286490a95e64803c89d6f1bad67c790a6f
Máté Szabó <mszabo@wikimedia.org>
2024-10-10 16:07:45 +0200

    FlaggablePageView: Add integration tests for showingStable()
    Why:
    
    - FlaggablePageView currently defaults to showing the stable version of
      an article (if such a stable version exists and was not explicitly
      opted out of) to anonymous users and the unstable version to registered
      users.
    - Since temporary accounts are considered to be registered users, this
      would cause them to be served the unstable version of pages by
      default.
    - Although there may be some sense in doing so, we'd like to keep
      backwards compatibility for now and serve the stable version of pages
      to temporary accounts.
    
    What:
    
    - Add integration tests for the relevant logic to confirm our
      assumptions about the behavior.
    
    Bug: T326934
    Change-Id: I43dcadf3a455aa15c7fe0bc2bd70693e93cbb9ff

---
+++ tests/phpunit/integration/FlaggablePageViewTest.php
@@ -0,0 +1,163 @@
+<?php
+
+use MediaWiki\Context\RequestContext;
+use MediaWiki\Request\FauxRequest;
+use MediaWiki\Revision\RevisionRecord;
+use MediaWiki\Tests\User\TempUser\TempUserTestTrait;
+use MediaWiki\User\User;
+
+/**
+ * @group Database
+ * @covers FlaggablePageView
+ */
+class FlaggablePageViewTest extends MediaWikiIntegrationTestCase {
+	use TempUserTestTrait;
+
+	private static WikiPage $testPage;
+	private static RevisionRecord $unstableRev;
+
+	public function addDBDataOnce() {
+		$this->overrideConfigValue( 'FlaggedRevsProtection', false );
+
+		self::$testPage = $this->getExistingTestPage();
+		$stableRev = self::$testPage->getRevisionRecord();
+
+		FlaggedRevs::autoReviewEdit(
+			self::$testPage,
+			$this->getTestSysop()->getUser(),
+			$stableRev
+		);
+
+		$pageUpdateStatus = $this->editPage( self::$testPage, 'unreviewed revision' );
+		self::$unstableRev = $pageUpdateStatus->getNewRevision();
+	}
+
+	/**
+	 * @dataProvider provideStableVersionOptions
+	 * @param Closure $userProvider
+	 * @param array $userOptions
+	 * @param string[] $queryParams
+	 * @return void
+	 */
+	public function testShouldShowStableVersion(
+		Closure $userProvider,
+		array $userOptions,
+		array $queryParams
+	): void {
+		$flaggablePageView = $this->getFlaggablePageView(
+			$userProvider->call( $this ),
+			$userOptions,
+			$queryParams
+		);
+
+		$this->assertTrue( $flaggablePageView->showingStable() );
+	}
+
+	public static function provideStableVersionOptions(): iterable {
+		// phpcs:disable Squiz.Scope.StaticThisUsage.Found
+		$registeredUserProvider = fn (): User => $this->getTestUser()->getUser();
+
+		yield 'anonymous user' => [
+			function (): User {
+				$this->disableAutoCreateTempUser();
+				return $this->getServiceContainer()->getUserFactory()->newAnonymous( '127.0.0.1' );
+			},
+			[],
+			[]
+		];
+
+		// phpcs:enable
+
+		yield 'registered user requesting stable version via preferences' => [
+			$registeredUserProvider,
+			[ 'flaggedrevsstable' => FR_SHOW_STABLE_ALWAYS ],
+			[]
+		];
+
+		yield 'registered user requesting stable version via query param' => [
+			$registeredUserProvider,
+			[ 'flaggedrevsstable' => FR_SHOW_STABLE_NEVER ],
+			[ 'stable' => '1' ]
+		];
+	}
+
+	/**
+	 * @dataProvider provideUnstableVersionOptions
+	 * @param Closure $userProvider
+	 * @param array $userOptions
+	 * @param Closure $queryParamProvider
+	 * @return void
+	 */
+	public function testShouldShowUnstableVersion(
+		Closure $userProvider,
+		array $userOptions,
+		Closure $queryParamProvider
+	): void {
+		$flaggablePageView = $this->getFlaggablePageView(
+			$userProvider->call( $this ),
+			$userOptions,
+			$queryParamProvider()
+		);
+
+		$this->assertFalse( $flaggablePageView->showingStable() );
+	}
+
+	/**
+	 * Convenience function to initialize a FlaggablePageView for a test page.
+	 *
+	 * @param User $user The user viewing the page.
+	 * @param string[] $userOptions Associative array of user options to set for the user.
+	 * @param string[] $queryParams Associative array of request query parameters to set.
+	 *
+	 * @return FlaggablePageView
+	 */
+	private function getFlaggablePageView(
+		User $user,
+		array $userOptions,
+		array $queryParams
+	): FlaggablePageView {
+		$request = new FauxRequest( $queryParams );
+
+		$context = new RequestContext();
+		$context->setUser( $user );
+		$context->setRequest( $request );
+
+		if ( $userOptions ) {
+			$userOptionsManager = $this->getServiceContainer()->getUserOptionsManager();
+			foreach ( $userOptions as $option => $value ) {
+				$userOptionsManager->setOption( $user, $option, $value );
+			}
+
+			$user->saveSettings();
+		}
+
+		$flaggablePageView = FlaggablePageView::newFromTitle( self::$testPage );
+		$flaggablePageView->setContext( $context );
+
+		return $flaggablePageView;
+	}
+
+	public static function provideUnstableVersionOptions(): iterable {
+		// phpcs:disable Squiz.Scope.StaticThisUsage.Found
+		$registeredUserProvider = fn (): User => $this->getTestUser()->getUser();
+		// phpcs:enable
+
+		yield 'registered user requesting unstable version via preferences' => [
+			$registeredUserProvider,
+			[ 'flaggedrevsstable' => FR_SHOW_STABLE_NEVER ],
+			static fn () => []
+		];
+
+		yield 'registered user requesting unstable version via stable param' => [
+			$registeredUserProvider,
+			[ 'flaggedrevsstable' => FR_SHOW_STABLE_ALWAYS ],
+			static fn () => [ 'stable' => '0' ]
+		];
+
+		yield 'registered user requesting unstable version via oldid param' => [
+			$registeredUserProvider,
+			[ 'flaggedrevsstable' => FR_SHOW_STABLE_ALWAYS ],
+			static fn () => [ 'oldid' => self::$unstableRev->getId() ]
+		];
+	}
+}
