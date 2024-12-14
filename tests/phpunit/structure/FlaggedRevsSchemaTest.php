4cfa4e86ffb133fb8c5d6e566e7c760e25795e5a
Daimona Eaytoy <daimona.wiki@gmail.com>
2024-12-14 03:55:23 +0100

    tests: Add structure test for abstract schema
    Use the new base class introduced in T381981.
    
    Also regenerate all the schema changes to get rid of drifts, as done in
    I3a3acc0935409d4601eb172664580b13d5fe2340.
    
    Depends-On: I8ae59ce6854922c2804d12986c870d3f0e0c3299
    Change-Id: I7626cf6840a708efa4a451cbcf2a52c714367764

---
+++ tests/phpunit/structure/FlaggedRevsSchemaTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace MediaWiki\Extensions\FlaggedRevs\Tests\Structure;
+
+use MediaWiki\Tests\Structure\AbstractSchemaTestBase;
+
+/**
+ * @coversNothing
+ */
+class FlaggedRevsSchemaTest extends AbstractSchemaTestBase {
+	protected static function getSchemasDirectory(): string {
+		return __DIR__ . '/../../../includes/backend/schema';
+	}
+
+	protected static function getSchemaChangesDirectory(): string {
+		return __DIR__ . '/../../../includes/backend/schema/abstractSchemaChanges/';
+	}
+
+	protected static function getSchemaSQLDirs(): array {
+		return [
+			'mysql' => __DIR__ . '/../../../includes/backend/schema/mysql',
+			'sqlite' => __DIR__ . '/../../../includes/backend/schema/sqlite',
+			'postgres' => __DIR__ . '/../../../includes/backend/schema/postgres',
+		];
+	}
+}
