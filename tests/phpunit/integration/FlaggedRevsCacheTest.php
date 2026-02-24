1cf035969025dcc5b45fe34dd5f5b95f48a74511
Isabelle Hurbain-Palatin <ihurbainpalatin@wikimedia.org>
2026-02-24 14:23:47 +0100

    Add a test to check FlaggedRevs cache behaviour
    This is a first step before adding the post-processing cache, in order
    to be able to test the post-processing cache behaviour as well.
    
    Bug: T414359
    Depends-On: I505fd4208d3b96d3f6f5c5c356614ce51ea4c48c
    Change-Id: I926f19b3ff1adf72c4845d381b87f228f7a1a7ac

---
+++ tests/phpunit/integration/FlaggedRevsCacheTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace MediaWiki\Extension\FlaggedRevs\Tests\Integration;
+
+use FlaggablePageView;
+use FlaggableWikiPage;
+use FlaggedRevs;
+use MediaWiki\Context\RequestContext;
+use MediaWiki\Parser\ParserCache;
+use MediaWiki\Parser\ParserCacheFactory;
+use MediaWiki\Request\FauxRequest;
+use MediaWiki\Tests\Parser\ParserCacheTestBase;
+use MediaWiki\Tests\Parser\TrackerWrapper;
+use MediaWiki\Tests\Parser\TrackingParserCache;
+
+/**
+ * @group Database
+ * @covers FlaggablePageView
+ */
+class FlaggedRevsCacheTest extends ParserCacheTestBase {
+
+	private TrackerWrapper $trackerWrapper;
+
+	public function setUp(): void {
+		parent::setUp();
+		$parserCacheFactory = $this->createMock( ParserCacheFactory::class );
+		$this->setService( 'ParserCacheFactory', $parserCacheFactory );
+
+		$this->trackerWrapper = new TrackerWrapper();
+		$caches = [];
+
+		$parserCacheFactory->method( 'getParserCache' )->willReturnCallback(
+			function ( $cacheName ) use ( &$caches ) {
+				$caches[$cacheName] ??= $this->getParserCache( $cacheName );
+				return $caches[$cacheName];
+			}
+		);
+	}
+
+	protected function createParserCache( ...$args ): ParserCache {
+		return new TrackingParserCache( $this->trackerWrapper, ...$args );
+	}
+
+	public function testCache() {
+		$this->setTemporaryHook(
+			'ParserOptionsRegister',
+			static function ( &$defaults, &$inCacheKey, &$lazyLoad ) {
+				$defaults['useParsoid'] = true;
+			}
+		);
+
+		$testPage = $this->getExistingTestPage();
+		$this->editPage( $testPage, 'hello' );
+		$stableRev = $testPage->getRevisionRecord();
+
+		FlaggedRevs::autoReviewEdit(
+			$testPage,
+			$this->getTestSysop()->getUser(),
+			$stableRev
+		);
+
+		$request = new FauxRequest( [ 'stable' => 1 ] );
+		$context = new RequestContext();
+		$context->setRequest( $request );
+
+		// clear local cache to update stable version in there
+		FlaggableWikiPage::getTitleInstance( $testPage->getTitle() )->clear();
+
+		$flaggablePageView = FlaggablePageView::newFromTitle( $testPage );
+		$flaggablePageView->setContext( $context );
+
+		// first call: nothing in cache
+		$parserOutput = null;
+		$useCache = true;
+		$flaggablePageView->setPageContent( $parserOutput, $useCache );
+		$this->assertArrayEquals( [ [ 'stable-parsoid-pcache', false ] ], $this->trackerWrapper->calls );
+
+		// second call: find it in the cache
+		$this->trackerWrapper->calls = [];
+		$parserOutput = null;
+		$useCache = true;
+		$flaggablePageView->setPageContent( $parserOutput, $useCache );
+		$this->assertArrayEquals( [ [ 'stable-parsoid-pcache', true ] ], $this->trackerWrapper->calls );
+	}
+}
